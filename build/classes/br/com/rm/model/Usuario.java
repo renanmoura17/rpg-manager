15c23b1fda3f5e4db966d38802b4adfafc634472
renanmoura17 <rmouraoliv@gmail.com>
2014-10-27 22:04:41 -0200

    Create Usuario.java

---
+++ build/classes/br/com/rm/model/Usuario.java
@@ -0,0 +1,33 @@
+package br.com.rm.model;
+
+public class Usuario {
+	private Long id;
+	private String login;
+	private String senha;
+	private int tipo;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	public int getTipo() {
+		return tipo;
+	}
+	public void setTipo(int tipo) {
+		this.tipo = tipo;
+	}
+}
