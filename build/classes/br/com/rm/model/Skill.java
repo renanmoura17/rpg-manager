fe4908cc9692328613e7e4ce33b2a89bf12cb046
renanmoura17 <rmouraoliv@gmail.com>
2014-10-31 14:46:35 -0200

    Create Skill.java

---
+++ build/classes/br/com/rm/model/Skill.java
@@ -0,0 +1,16 @@
+package br.com.rm.model;
+
+public class Skill {
+	private String nome;
+	private String descricao;
+	private Integer mp_gasto;
+	
+	public Skill (String nome, String descricao, Integer mp_gasto) {
+		this.nome = nome;
+		this.descricao = descricao;
+		this.mp_gasto = mp_gasto;		
+	}
+	
+	//Getters and Setters...	
+
+}
