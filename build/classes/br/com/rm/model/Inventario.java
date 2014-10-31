e7c7c341afd03caeddee64774cce868c070a809a
renanmoura17 <rmouraoliv@gmail.com>
2014-10-31 14:48:31 -0200

    Create Inventario.java

---
+++ build/classes/br/com/rm/model/Inventario.java
@@ -0,0 +1,12 @@
+package br.com.rm.model;
+
+public class Inventario {
+	private Integer tamanho_max;
+	
+	public Skill (Integer tamanho_max) {
+		this.tamanho_max = tamanho_max;	
+	}
+	
+	//Getters and Setters...	
+
+}
