7b9bef0ed17cc71e1971eddfb4e3082638f0d43a
renanmoura17 <rmouraoliv@gmail.com>
2014-10-31 15:19:20 -0200

    Create Item.java

---
+++ build/classes/br/com/rm/model/Item.java
@@ -0,0 +1,15 @@
+package br.com.rm.model;
+
+public class Item {
+	private String nome;
+	private String descricao;
+	private Integer slots_ocupados;
+	
+	public Item (String nome, String, descricao, Integer slots_ocupados) {
+		this.nome = nome;
+		this.descricao = descricao;
+		this.slots_ocupados = slots_ocupados;
+	}
+	
+	//Getters and Setters...
+}
